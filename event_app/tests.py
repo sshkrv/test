6ac5f9c1811e6f7cfcb7525ebdfb0a9e9f0e6ccf
Sabir <sabir@sneaker.sale>
2023-07-14 12:49:53 +0200

    added negative scenario tests

--- event_app/tests.py
+++ event_app/tests.py
@@ -5,25 +5,45 @@ from rest_framework import status
 
 
 class TestUser(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpass')
-
     def test_user_registration(self):
-        data = {'username': 'testuser2', 'password': 'testpass2'}
+        data = {'username': 'testuser', 'password': 'testpass'}
         url = reverse('register')
 
         response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 2)
-        self.assertEqual(User.objects.get(username='testuser2').username, 'testuser2')
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get(username='testuser').username, 'testuser')
+
+    def test_user_registration_with_already_taken_username(self):
+        User.objects.create_user(username='testuser', password='testpass')
+
+        data = {'username': 'testuser', 'password': 'testpass2'}
+        url = reverse('register')
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_user_login(self):
+        User.objects.create_user(username='testuser', password='testpass')
+
         data = {'username': 'testuser', 'password': 'testpass'}
-        url = reverse('token_obtain_pair')
+        url = reverse('login')
 
         response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue('access' in response.data)
         self.assertTrue('refresh' in response.data)
+
+    def test_user_login_with_wrong_credentials(self):
+        User.objects.create_user(username='testuser', password='testpass')
+
+        data = {'username': 'testuser', 'password': 'wrongpass'}
+        url = reverse('login')
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
